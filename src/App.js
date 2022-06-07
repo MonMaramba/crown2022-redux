3455274c331a014b4eae6b3c427a1ccc1bcccbb5
MonMaramba <monmaramba@yahoo.com>
2022-06-06 18:59:23 -0700

    using Fragment to implement Outlet. Link is also used from react-router-dom. Routing working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
+import Navigation from './routes/home/navigation/navigation.component';
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>I am the shop component</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Home />}></Route>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
     </Routes>
   );
 }
