02fa91b007d22317e0a832ba787a450dc1996ea7
MonMaramba <monmaramba@yahoo.com>
2022-06-15 18:41:44 -0700

    added GoogleSignInWithRedirect and passed along results back to home page

--- src/routes/sign-in/sign-in.component.js
+++ src/routes/sign-in/sign-in.component.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import { getRedirectResult } from 'firebase/auth';
+import React, { useEffect } from 'react';
 import {
   signInWithGooglePopup,
   createUserDocumentFromAuth,
+  signInWithGoogleRedirect,
+  auth,
 } from '../../utils/firebase.utils';
 
 const SignIn = () => {
+  useEffect(() => {
+    async function _getRedirectResult() {
+      const response = await getRedirectResult(auth);
+      console.log(response);
+    }
+    _getRedirectResult();
+  }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
@@ -13,6 +24,9 @@ const SignIn = () => {
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign In With Google</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign In With Google Redirect
+      </button>
     </div>
   );
 };
