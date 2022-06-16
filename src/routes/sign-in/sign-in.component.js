2bd2bf0646033bdaa9e4f23635075daece83177f
MonMaramba <monmaramba@yahoo.com>
2022-06-15 19:22:21 -0700

    sign-up form completed plus useState for updating formFields

--- src/routes/sign-in/sign-in.component.js
+++ src/routes/sign-in/sign-in.component.js
@@ -1,21 +1,12 @@
-import { getRedirectResult } from 'firebase/auth';
-import React, { useEffect } from 'react';
+import React from 'react';
 import {
   signInWithGooglePopup,
   createUserDocumentFromAuth,
-  signInWithGoogleRedirect,
-  auth,
 } from '../../utils/firebase.utils';
 
-const SignIn = () => {
-  useEffect(() => {
-    async function _getRedirectResult() {
-      const response = await getRedirectResult(auth);
-      console.log(response);
-    }
-    _getRedirectResult();
-  }, []);
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 
+const SignIn = () => {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
@@ -24,9 +15,7 @@ const SignIn = () => {
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign In With Google</button>
-      <button onClick={signInWithGoogleRedirect}>
-        Sign In With Google Redirect
-      </button>
+      <SignUpForm />
     </div>
   );
 };
