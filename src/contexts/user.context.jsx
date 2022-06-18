52fc45bb99156067a053603e3cde511ac7f3dd24
MonMaramba <monmaramba@yahoo.com>
2022-06-18 13:37:12 -0700

    React context set up in navigation, index and sign-in

---
+++ src/contexts/user.context.jsx
@@ -0,0 +1,16 @@
+import { createContext, useState } from 'react';
+
+// as actual value you want o access
+export const UserContext = createContext({
+  currentUser: null,
+  setCurrentUser: () => null,
+});
+
+// Provider - the actual component
+export const UserProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const value = { currentUser, setCurrentUser };
+  return (
+    <UserContext.Provider value={value}> {children} </UserContext.Provider>
+  );
+};
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,10 +1,15 @@
-import { Fragment } from 'react';
+import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
+
+import { UserContext } from '../../contexts/user.context';
+
 import './navigation.styles.scss';
 
 import { ReactComponent as Crwnlogo } from '../../assets/crown.svg';
 
 const Navigation = () => {
+  const { currentUser } = useContext(UserContext);
+  // console.log(currentUser);
   return (
     <Fragment>
       <div className='navigation'>
