257d4c9da6e3dd8327a544a1aac979b074ee06bc
MonMaramba <monmaramba@yahoo.com>
2022-06-21 19:32:32 -0700

    created checkout route, component and linked dropdown cart button

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import { Link } from 'react-router-dom';
 
 import { CartContext } from '../../contexts/cart.context';
 
@@ -17,7 +18,9 @@ const CartDropdown = () => {
           <CartItem key={item.id} cartItem={item} />
         ))}
       </div>
-      <Button>GO TO CHECKOUT</Button>
+      <Button>
+        <Link to='/checkout'>GO TO CHECKOUT</Link>
+      </Button>
     </div>
   );
 };
