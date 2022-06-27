0d72ac43ff633d7d676664ec4f0225df7f85430f
MonMaramba <monmaramba@yahoo.com>
2022-06-27 16:47:28 -0700

    shop data moved to Firestore in one-off operation

--- src/utils/firebase.utils.js
+++ src/utils/firebase.utils.js
@@ -8,7 +8,14 @@ import {
   signInWithEmailAndPassword,
   onAuthStateChanged,
 } from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  writeBatch,
+} from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyAuMZKahq5EUJBPhXRHo8y1NKYwKGp33fo',
@@ -33,6 +40,23 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 
 // instantiating firestore to access database
 export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = collection(db, collectionKey);
+  const batch = writeBatch(db);
+
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object);
+  });
+
+  await batch.commit();
+  console.log('done');
+};
+
 // CRUD on Firestore
 
 export const createUserDocumentFromAuth = async (
