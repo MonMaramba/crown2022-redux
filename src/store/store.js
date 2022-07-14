ff326a6ecfd58be1e7b4691475b625f18cf5a381
MonMaramba <monmaramba@yahoo.com>
2022-07-14 16:56:14 -0700

    added store and wrapped everything in Provider with store passed in

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,15 @@
-import { compose, createStore, applyMiddleware } from 'redux';
+import {
+  compose,
+  legacy_createStore as createStore,
+  applyMiddleware,
+} from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 
-// root-reducer
+import { rootReducer } from './root-reducer';
+
+const middleWares = [logger];
+
+const composedEnhancers = compose(applyMiddleware(...middleWares));
+
+export const store = createStore(rootReducer, undefined, composedEnhancers);
