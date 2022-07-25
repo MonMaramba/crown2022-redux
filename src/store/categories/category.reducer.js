d91377a3ae141140a54f02c2dc61a44febf6f90b
MonMaramba <monmaramba@yahoo.com>
2022-07-24 18:05:03 -0700

    Made some optimizations to prevent re-renders in category component

--- src/store/categories/category.reducer.js
+++ src/store/categories/category.reducer.js
@@ -4,12 +4,15 @@ const CATEGORIES_INITIAL_STATE = {
   categories: [],
 };
 
-export const categoriesReducer = (state = CATEGORIES_INITIAL_STATE, action) => {
+export const categoriesReducer = (
+  state = CATEGORIES_INITIAL_STATE,
+  action = {}
+) => {
   const { type, payload } = action;
 
   switch (type) {
     case CATEGORIES_ACTION_TYPES.SET_CATEGORIES:
-      return { ...state, categoriesMap: payload };
+      return { ...state, categories: payload };
 
     default:
       return state;
